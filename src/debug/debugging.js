e98ae98571f5b48ca4b82625cd432fce5e8d4295
wilyn Rodriguez <wrodriguezl@unitru.edu.pe>
2025-09-02 01:43:26 -0500

    wilyn

---
+++ src/debug/debugging.js
@@ -0,0 +1,8 @@
+var  a = 'hello';
+
+function hello(){
+    let b = 'hello world';
+    const c = 'hello world!'
+    debugger;
+}
+hello();
--- src/hosting/hosting.js
+++ src/hosting/hosting.js
@@ -1,2 +1,14 @@
+/* var nameOfDog;  // valor undefined
 console.log(nameOfDog);
 var nameOfDog = 'elmo';
+console.log(nameOfDog); */
+
+var elmo; //se agrega aqui como undefined;
+nameOfDog();
+
+function nameOfDog(){
+    console.log(`El mejor perrito es: ${elmo}`);
+}
+var elmo = 'Sol y Odie';
+
+nameOfDog();
